bd18551f392d41f2af697fffbeb02067b0f72ce0
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-12-07 21:37:34 -0600

    WIP welcome window

---
+++ Service Manual/WelcomeWindowController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeWindowController.h
+//  Service Manual
+//
+//  Created by Jason Whitehorn on 12/7/12.
+//  Copyright (c) 2012 Jason Whitehorn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WelcomeWindowController : NSWindowController
+
+@end
